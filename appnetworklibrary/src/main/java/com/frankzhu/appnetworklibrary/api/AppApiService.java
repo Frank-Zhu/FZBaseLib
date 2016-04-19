a4ee4ea78fc02be6b13675d4e4071a4377822e56
祝文武 <zhuwenwu2008@gmail.com>
2016-04-19 18:34:24 +0800

    添加数据结构

---
+++ appnetworklibrary/src/main/java/com/frankzhu/appnetworklibrary/api/AppApiService.java
@@ -0,0 +1,15 @@
+package com.frankzhu.appnetworklibrary.api;
+
+/**
+ * Author:    ZhuWenWu
+ * Version    V1.0
+ * Date:      16/4/19 下午6:09
+ * Description:
+ * Modification  History:
+ * Date         	Author        		Version        	Description
+ * -----------------------------------------------------------------------------------
+ * 16/4/19      ZhuWenWu            1.0                    1.0
+ * Why & What is modified:
+ */
+public interface AppApiService {
+}
